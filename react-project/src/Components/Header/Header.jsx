9dfc6250a3cb3bee7a730be4d3cce43a83c78d4c
Thxsenseii <Thxsenseii@gmail.com>
2024-04-24 12:59:42 +0100

    started css

--- react-project/src/Components/Header/Header.jsx
+++ react-project/src/Components/Header/Header.jsx
@@ -5,10 +5,6 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <div id="headerContainer">
-        {/* <h2 id="headerTitle">Reboot Cocktails</h2> */}
-        {/* <Link to="/"><p>Home</p></Link> */}
-        {/* <Link  reloadDocument  to="random"><p>Random</p></Link> */}
-        {/* <Link to="categories"><p>Categories</p></Link> */}
     </div>
   )
 }
--- react-project/src/Router/index.jsx
+++ react-project/src/Router/index.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import Root from '../Layout/Root/index.jsx'
-// import Home from '../Pages/Home/Home'
+import Home from '../Pages/Home/Home.jsx'
 import SignUp from '../Pages/SignUp/SignUp.jsx'
 import LogIn from '../Pages/LogIn/LogIn.jsx'
 
@@ -11,10 +11,10 @@ const router = createBrowserRouter([
       path: '/',
       element: <Root />,
       children: [
-        // {
-        //   path: '',
-        //   element: <Home />
-        // },
+        {
+          path: '/',
+          element: <Home />
+        },
         {
             path:'/SignUp',
             element: <SignUp />
