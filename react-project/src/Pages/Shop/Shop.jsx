faa28929bdbe2424f19df4603595572f504ede41
Julio <julio_bh@hotmail.es>
2024-04-30 14:48:19 +0100

    carrito

--- react-project/src/Pages/Shop/Shop.jsx
+++ react-project/src/Pages/Shop/Shop.jsx
@@ -1,37 +1,38 @@
 import React from 'react';
 import './Shop.css';
-import { useCart } from '../../Components/Cart/CartContext.jsx'; // Importa el hook useCart
+import { useCart } from '../../Components/Cart/CartContext.jsx';
 
 const Shop = () => {
-  const { cart, clearCart } = useCart(); // Usa el hook useCart para acceder al carrito
+  const { cart, clearCart } = useCart();
+  const totalPrice = cart.reduce((total, producto) => total + producto.price, 0);
 
   return (
     <div className="shop-container">
       <h1>Carrito de Compras</h1>
-      {/* Aquí puedes mostrar los detalles del carrito, como los productos y el total */}
-      <div>
-        <h2>Productos en el carrito:</h2>
+      <div className="products-grid">
         {cart.length > 0 ? (
-          <div>
-            {cart.map((product, index) => (
-              <div key={index} className="cart-item">
-                <img src={product.image} alt={`Product ${index}`} className="product-image" />
-                <div className="product-details">
-                  <p>{product.product_name}</p>
-                  <p>Precio: {product.price} €</p>
-                </div>
+          cart.map((product, index) => (
+            <div key={index} className="product-card">
+              <img src={product.image} alt={`Product ${index}`} className="product-image" />
+              <div className="product-details">
+                <p>{product.product_name}</p>
+                <p>Precio: {product.price} €</p>
               </div>
-            ))}
-            <p>Total Items: {cart.length}</p>
-            <button onClick={clearCart}>Vaciar Carrito</button>
-          </div>
+            </div>
+          ))
         ) : (
           <p>El carrito está vacío.</p>
         )}
       </div>
+      {cart.length > 0 && (
+        <div>
+          <p>Total Items: {cart.length}</p>
+          <p>Total: {totalPrice} €</p>
+          <button onClick={clearCart}>Vaciar Carrito</button>
+        </div>
+      )}
     </div>
   );
 };
 
 export default Shop;
-
