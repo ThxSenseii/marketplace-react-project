ebd37ca377aa8097cf18ad2ca3130c906581becf
SeryiSH94 <seryish94@gmail.com>
2024-04-30 16:50:52 +0100

    Frontent casi listo

--- react-project/src/Pages/Shop/Shop.jsx
+++ react-project/src/Pages/Shop/Shop.jsx
@@ -1,43 +1,3 @@
-/* import React from 'react';
-import './Shop.css';
-import { useCart } from '../../Components/Cart/CartContext.jsx';
-
-const Shop = () => {
-  const { cart, clearCart } = useCart();
-  const totalPrice = cart.reduce((total, producto) => total + producto.price, 0);
-
-  return (
-    <div className="shop-container">
-      <h1>Carrito de Compras</h1>
-      <div className="products-grid">
-        {cart.length > 0 ? (
-          cart.map((product, index) => (
-            <div key={index} className="product-card">
-              <img src={product.image} alt={`Product ${index}`} className="product-image" />
-              <div className="product-details">
-                <p>{product.product_name}</p>
-                <p>Precio: {product.price} €</p>
-              </div>
-            </div>
-          ))
-        ) : (
-          <p>El carrito está vacío.</p>
-        )}
-      </div>
-      {cart.length > 0 && (
-        <div>
-          <p>Total Items: {cart.length}</p>
-          <p>Total: {totalPrice} €</p>
-          <button onClick={clearCart}>Vaciar Carrito</button>
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default Shop;
- */
-
 import React from 'react';
 import './Shop.css';
 import { useCart } from '../../Components/Cart/CartContext.jsx';
@@ -64,13 +24,13 @@ const Shop = () => {
             </div>
           ))
         ) : (
-          <p>El carrito está vacío.</p>
+          <div className="empty-cart-message"><p>El carrito está vacío.</p></div>
         )}
       </div>
       {cart.length > 0 && (
         <div>
-          <div className='total'><p>Total Items: {cart.length}</p></div>
-          <div className='total'><p>Total: {totalPrice} €</p></div>
+          <div className='total-info'><p>Total Items: {cart.length}</p></div>
+          <div className='total-info'><p>Total: {totalPrice} €</p></div>
           <button onClick={clearCart}>Vaciar Carrito</button>
         </div>
       )}
