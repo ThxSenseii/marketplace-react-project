a1873175dc28c724e4a146ec966721abd6288c7e
SeryiSH94 <seryish94@gmail.com>
2024-05-02 13:24:19 +0100

    pagina funcional

--- react-project/src/Pages/SignUp/SignUp.jsx
+++ react-project/src/Pages/SignUp/SignUp.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+/* import * as React from 'react';
 import { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
@@ -160,6 +160,175 @@ function SignUp() {
   );
 }
 
+export default SignUp; */
+
+import * as React from 'react';
+import { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { signupp } from '../../services/auth';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from 'react-router-dom';
+
+function SignUp() {
+  const [registered, setRegistered] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    let dataSignUp = await signupp({
+      email: data.get('email'),
+      password: data.get('password'),
+      user_name: data.get('user_name'),
+      address: data.get('address'),
+      mobil_phone: data.get('mobil_phone')
+    });
+    
+    if (dataSignUp.message){
+      alert(dataSignUp.message);
+    }
+    else{
+      alert('Registration successful')
+      localStorage.setItem("token", dataSignUp.userData.token);
+    localStorage.setItem("useremail", dataSignUp.userData.email);
+    localStorage.setItem("userid", dataSignUp.userId);
+    localStorage.setItem("address", dataSignUp.userData.address);
+    localStorage.setItem("mobil_phone", dataSignUp.userData.phone);
+    localStorage.setItem("user_name", dataSignUp.userData.name);
+    }
+
+    return dataSignUp;
+  };
+
+  // Redirigir al usuario a la página "user" después de registrarse
+  React.useEffect(() => {
+    if (registered) {
+      navigate('/User');
+    }
+  }, [registered, navigate]);
+
+  return (
+    <ThemeProvider theme={createTheme()}>
+      <Container component="main" maxWidth="xs" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh', marginLeft: 'auto', marginRight: 'auto' }}>
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            backgroundColor: 'white',
+            padding: '20px',
+            borderRadius: '8px',
+            boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)'
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 3, width: '100%' }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="fname"
+                  name="user_name"
+                  required
+                  fullWidth
+                  id="user_name"
+                  label="User Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="address"
+                  label="Address"
+                  type="text"
+                  id="address"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="mobil_phone"
+                  label="Mobile Phone"
+                  type="text"
+                  id="mobil_phone"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/login" variant="body2">
+                  Already have an account? Log in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
 export default SignUp;
+
+
+
+
+
 
 
